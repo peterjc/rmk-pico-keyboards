064aa5d08298eac2d1140e4dfe3e6df7957194bc
Peter J. A. Cock <p.j.a.cock@googlemail.com>
2026-05-22 12:41:25 +0900

    Revert "Automate per-keyboard workflows"
    This reverts commit d16418281d30b953410ae3eaa6c481b2b41f2439.

--- .github/workflows/update.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -euo pipefail
-for NAME in ../../*/keyboard.toml; do
-    NAME=${NAME%/keyboard.toml}
-    NAME=${NAME#\.\./\.\./}
-    echo $NAME
-    cat build.template | sed "s/\$NAME/$NAME/g" > $NAME.yaml
-done
